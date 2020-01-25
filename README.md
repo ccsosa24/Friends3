1d0ded450f22ff8c89e9737c9aa605041d9c9c11
ccsosa24 <chrstina.sosa024@gmail.com>
2020-01-25 13:24:17 -0700

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Friends3-Clicky-Game
-
-Click on an image to gain points, however, if you click on the same image in a row you lose!!!
-
-![friendsgame](friends3-clicky-game/src/components/Images/friendsgame.jpg)
---
+++ precache-manifest.0fdd9c01592c3760017de8bc00f7d12e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408dbe827bcaaa77a3deb980c04265ff",
+    "url": "/Friends3/index.html"
+  },
+  {
+    "revision": "e7ecf5d472d140b98091",
+    "url": "/Friends3/static/css/main.5fcaaf50.chunk.css"
+  },
+  {
+    "revision": "968cef40f4a697ca02c0",
+    "url": "/Friends3/static/js/2.9bb941be.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Friends3/static/js/2.9bb941be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7ecf5d472d140b98091",
+    "url": "/Friends3/static/js/main.4a97c088.chunk.js"
+  },
+  {
+    "revision": "a1a81cbb6069188e7e6a",
+    "url": "/Friends3/static/js/runtime-main.aef79c46.js"
+  }
+]);
